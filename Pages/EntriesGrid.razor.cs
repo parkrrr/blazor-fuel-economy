f31e648581ea8bf3b99b310129a21b1ae1f370d9
Parker Smith <parkerbsmith@gmail.com>
2024-05-05 21:08:35 -0400

    Add entries grid to detail

---
+++ Pages/EntriesGrid.razor.cs
@@ -0,0 +1,108 @@
+﻿using FuelEconomy.Model;
+using FuelEconomy.Services;
+using Microsoft.AspNetCore.Components;
+using Radzen.Blazor;
+
+namespace FuelEconomy.Pages
+{
+    public partial class EntriesGrid
+    {
+        [Inject]
+        public EntriesService EntriesService { get; set; } = null!;
+
+        [Inject]
+        public VehicleService VehicleService { get; set; } = null!;
+
+        [Parameter]
+        public Vehicle? Vehicle { get; set; }
+
+        private RadzenDataGrid<Entry> _grid = null!;
+
+        private List<Entry> _data = new();
+        private List<Vehicle> _vehicles = new();
+
+        private Entry? _currentlyEditing;
+
+        void Reset()
+        {
+            _currentlyEditing = null;
+        }
+
+        async Task EditRow(Entry entry)
+        {
+            _currentlyEditing = entry;
+            await _grid.EditRow(entry);
+        }
+
+        async Task OnUpdateRow(Entry entry)
+        {
+            Reset();
+
+            await EntriesService.UpdateAsync(entry);
+        }
+
+        async Task SaveRow(Entry entry)
+        {
+            await _grid.UpdateRow(entry);
+        }
+
+        void CancelEdit(Entry entry)
+        {
+            Reset();
+
+            _grid.CancelEditRow(entry);
+        }
+
+        async Task DeleteRow(Entry entry)
+        {
+            Reset();
+
+            if (_data.Contains(entry))
+            {
+                _data.Remove(entry);
+                await EntriesService.RemoveAsync(entry);
+                await _grid.Reload();
+            }
+            else
+            {
+                _grid.CancelEditRow(entry);
+                await _grid.Reload();
+            }
+        }
+
+        async Task InsertRow(Vehicle? vehicle)
+        {
+            if (vehicle == null)
+            {
+                return;
+            }
+
+            _currentlyEditing = new Entry(vehicle);
+            await _grid.InsertRow(_currentlyEditing);
+        }
+
+        async Task OnCreateRow(Entry entry)
+        {
+            await EntriesService.AddAsync(entry);
+            _currentlyEditing = null;
+        }
+
+        private void OnVehicleChange(Vehicle? vehicle)
+        {
+            Vehicle = vehicle;
+
+            if (Vehicle != null)
+            {
+                _data = EntriesService.Get(Vehicle);
+            }
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            _vehicles = VehicleService.Get();
+            OnVehicleChange(_vehicles.FirstOrDefault());
+        }
+    }
+}
