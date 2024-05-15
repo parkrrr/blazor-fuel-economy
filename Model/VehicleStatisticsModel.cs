b15dc2d4c3f28bc9ed5095fdfb00a88a1c59fbd3
Parker Smith <parkerbsmith@gmail.com>
2024-05-15 16:50:45 -0400

    Use "distance" instead of "miles", remove unused statistics

--- Model/VehicleStatisticsModel.cs
+++ Model/VehicleStatisticsModel.cs
@@ -2,7 +2,7 @@
 {
     public record VehicleStatisticsModel
     {
-        public decimal TotalMiles { get; set; }
+        public decimal TotalDistance { get; set; }
         public decimal TotalCost { get; set; }
         public DateTime LatestDate { get; set; }
         public DateTime InitialDate { get; set; }
@@ -12,24 +12,14 @@
         public decimal WorstEconomy { get; set; }
         public decimal MedianEconomy { get; set; }
 
-        public decimal SummerEconomy { get; set; }
-        public decimal SpringEconomy { get; set; }
-        public decimal FallEconomy { get; set; }
-        public decimal WinterEconomy { get; set; }
-
         public decimal HighestCost { get; set; }
         public decimal LowestCost { get; set; }
 
-        public decimal AverageSummerCost { get; set; }
-        public decimal AverageSpringCost { get; set; }
-        public decimal AverageFallCost { get; set; }
-        public decimal AverageWinterCost { get; set; }
-
         public decimal CostPerDay { get; set; }
-        public decimal CostPerMile { get; set; }
-        public decimal CostPer100Miles { get; set; }
+        public decimal CostPerDistance { get; set; }
+        public decimal CostPer100Distance { get; set; }
 
-        public decimal AverageYearlyMiles { get; set; }
+        public decimal AverageYearlyDistance { get; set; }
         public decimal AverageYearlyCost { get; set; }
     }
 }
